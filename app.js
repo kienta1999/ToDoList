e07e44863f156e704194c845025b6439c93421cc
Kien Ta <talekien1710@gmail.com>
2020-09-01 09:41:19 +0700

    mongoose installed and items added to database

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dateTime = require(__dirname + "/date.js");
+
 app.set('view engine', 'ejs');
+mongoose.connect("mongodb://localhost:27017/todolistDB",  { useNewUrlParser: true,  useUnifiedTopology: true});
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+const itemSchema = {
+  name: String
+};
+
+//Item collection and the schema to create collection
+const Item = mongoose.model("Item", itemSchema);
 
 const toDoList = ["apply", "study"];
 
 const port = 3000;
 
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date + ' ' + time;
+
 
 app.get('/', (req, res) => {
-  
+  const item1 = new Item({
+    name: "apply"
+  });
+  const item2 = new Item({
+    name: "study"
+  });
+
+  Item.insertMany([item1, item2], function(err){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log("Successfully add default items to database");
+    }
+  })
   res.render("list", {day: dateTime, toDoList: toDoList});
   // res.sendFile("index.html");
 });
