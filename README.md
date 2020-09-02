a8271b935c7557219c5ec314c3b60723f0b0eb9b
Kien Ta <talekien1710@gmail.com>
2020-09-02 10:17:16 +0700

    add listSchema and modify README

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # ToDoList
 
-init npm and install express, body-parser, ejs, and mongoose in your local directory. 
-Open the to do list at http://localhost:3000/ and add the items as you wish.
+Make sure you have Node.js and mongoDB installed.
+
+Step 1: Navigate to the following path and run command "npm i" to install the needed Node.js framework (express, body-parser, ejs, and mongoose)
+
+Step 2: Direct to the bin folder of mongoDB "C:\Program Files\MongoDB\Server\4.4\bin" (replace 4.4 with your MongoDB version). Run mongod.exe (to activate the database).
+
+Step 3: Open the to do list at http://localhost:3000/ and add the items as you wish.
--- app.js
+++ app.js
@@ -14,19 +14,26 @@ const itemSchema = {
   name: String
 };
 
+const listSchema = {
+  name: String,
+  items: [itemSchema]
+}
+
 //Item collection and the schema to create collection
 const Item = mongoose.model("Item", itemSchema);
+const List = mongoose.model("List", listSchema);
 
 const port = 3000;
+const item1 = new Item({
+  name: "apply"
+});
+const item2 = new Item({
+  name: "study"
+});
+defaultItems = [item1, item2];
 
-async function addDefaultDb(){
-  const item1 = new Item({
-    name: "apply"
-  });
-  const item2 = new Item({
-    name: "study"
-  });
-  Item.insertMany([item1, item2], function(err){
+function addDefaultDb(){
+  Item.insertMany(defaultItems, function(err){
     if(err){
       console.log(err);
     }
@@ -57,6 +64,17 @@ app.get('/', (req, res) => {
   
   // res.sendFile("index.html");
 });
+
+app.get("/:customListName", (req, res) => {
+  const customListName = req.params.customListName;
+  const list = new List({
+    name: customListName,
+    item = defaultItems
+  });
+  list.save();
+  res.send(list);
+});
+
 
 app.post('/', (req, res) =>{
   // res.send('POST request to the homepage')
